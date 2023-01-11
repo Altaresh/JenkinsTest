e3e9ff59b51ef72e31302bb66c9f8b2e2346b50b
Dmitriy-Demianov <Dmitriy.Demianov@outlook.com>
2023-01-11 06:02:22 +0200

    initial

---
+++ src/JenkinsTest.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JenkinsTest.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
